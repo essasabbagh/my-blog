bd1ee7395b3ddb644a038c77b1c64e3095531b06
Essa Sabbagh <50638748+essasabbagh@users.noreply.github.com>
2024-08-28 13:33:44 +0300

    Setup base blog

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CategoryController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+    // routes/web.php
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('/post/{slug}', [PostController::class, 'show'])->name('posts.show');
+Route::get('/category/{slug}', [CategoryController::class, 'show'])->name('category.show');
+Route::get('/tag/{slug}', [TagController::class, 'show'])->name('tag.show');
+
+require __DIR__.'/auth.php';
